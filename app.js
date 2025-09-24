350a32dc11480ea713bff3ec471b5ef9646e0c34
DuLu10-2592 <pabnahu53@gmail.com>
2025-09-23 23:08:40 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+let amigos = [];
+
+function agregaAmigo() {
+    let nombre = document.querySelector('#amigo');
+    
+    if (nombre.value == "") {
+        alert("Ingrese un nombre, por favor");
+        agregaAmigo;
+    } else {
+        amigos.push(nombre.value);
+        let lista = document.getElementById("listaAmigos");
+        lista.innerHTML = ""; //limpia la lista antes de actualizar
+        
+        amigos.forEach(elemento=> {
+            let item = document.createElement("li");
+            item.textContent=elemento;
+            lista.appendChild(item);
+        })
+    }
+    document.querySelector('#amigo').value = '';
+};
+
+function sortearAmigo(){
+    let indiceAleatorio = Math.floor(Math.random()*amigos.length);
+    let elegido = amigos[indiceAleatorio];
+    
+                amigos.splice(indiceAleatorio,1);
+    
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    return asignarTextoElemento("#resultado", elegido);
+};
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+};
